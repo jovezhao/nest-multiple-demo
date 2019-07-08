0ee546f66f97fbc4661517663094e9d93f2877ee
jovezhao <jovezhao@foxmail.com>
2019-07-08 16:13:03 +0800

    测试applicationcontext

--- demo-application/src/main/java/com/guoshouxiang/nest/demo/api/v1/controllers/TestController.java
+++ demo-application/src/main/java/com/guoshouxiang/nest/demo/api/v1/controllers/TestController.java
@@ -4,6 +4,7 @@ import com.guoshouxiang.nest.demo.contract.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class TestController {
 
     @Autowired
     private UserService userService;
+
 
     @ApiOperation("version")
     @GetMapping("/version")
